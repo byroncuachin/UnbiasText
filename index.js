f52f5ae51c43a9693bb2becd58d8174e9823255c
Byron Josh Cuachin <byroncuachin@gmail.com>
2024-04-15 21:51:47 -0700

    Fixed problem with firebase hsoting not being applied

--- index.js
+++ index.js
@@ -1,166 +1,23 @@
-// const express = require("express");
-// const cors = require("cors");
-// const axios = require("axios");
-// const { readFile } = require("fs").promises;
-
-// const app = express();
-
-// app.use(cors()); // Enable CORS
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static("public"));
-
-// app.get("/", async (req, res) => {
-//   res.send(await readFile("./home.html", "utf8"));
-// });
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`App is available on http://localhost:${PORT}`);
-// });
-
-// app.post("/", async (req, res) => {
-//   const userInput = req.body.text; // Access the text input from the request body
-
-//   // Process the data (e.g., you can log it)
-//   console.log("Received text:", userInput);
-
-//   try {
-//     // prediction API call
-//     const predictionResponse = await axios.post(
-//       "http://127.0.0.1:5000/predict",
-//       {
-//         text: userInput,
-//       }
-//     );
-//     console.log("Prediction data:", predictionResponse.data);
-//     console.log("Male Percentage:", predictionResponse.data.malePercentage);
-//     console.log("Masculine Words", predictionResponse.data.masculineWords);
-//     console.log("Female Percentage:", predictionResponse.data.femalePercentage);
-//     console.log("Feminine Words", predictionResponse.data.feminineWords);
-//     console.log(
-//       "Original Masculine Words:",
-//       predictionResponse.data.originalMasculineWords
-//     );
-//     console.log(
-//       "Original Feminine Words:",
-//       predictionResponse.data.originalFeminineWords
-//     );
-
-//     const m_percent = predictionResponse.data.malePercentage * 100;
-//     const f_percent = predictionResponse.data.femalePercentage * 100;
-//     const m_highlight = predictionResponse.data.originalMasculineWords;
-//     const f_highlight = predictionResponse.data.originalFeminineWords;
-//     const m_table = predictionResponse.data.masculineWords;
-//     const f_table = predictionResponse.data.feminineWords;
-//     // Send a response
-//     res.status(200).send({
-//       message: "Text submitted successfully",
-//       userInput,
-//       malePercentage: m_percent,
-//       femalePercentage: f_percent,
-//       maleHighlight: m_highlight,
-//       femaleHighlight: f_highlight,
-//       maleTable: m_table,
-//       femaleTable: f_table,
-//     });
-
-//     // if (typeof scrollBar !== "undefined") {
-//     //   if (scrollBar === 0) {
-//     //     // scrollBar is at Gender
-//     //     // Print masculine-feminine log messages
-//     //     console.log("Male Percentage:", predictionResponse.data.malePercentage);
-//     //     console.log("Masculine Words", predictionResponse.data.masculineWords);
-//     //     console.log(
-//     //       "Female Percentage:",
-//     //       predictionResponse.data.femalePercentage
-//     //     );
-//     //     console.log("Feminine Words", predictionResponse.data.feminineWords);
-//     //     console.log(
-//     //       "Original Masculine Words:",
-//     //       predictionResponse.data.originalMasculineWords
-//     //     );
-//     //     console.log(
-//     //       "Original Feminine Words:",
-//     //       predictionResponse.data.originalFeminineWords
-//     //     );
-
-//     //     const m_percent = predictionResponse.data.malePercentage * 100;
-//     //     const f_percent = predictionResponse.data.femalePercentage * 100;
-//     //     const m_highlight = predictionResponse.data.originalMasculineWords;
-//     //     const f_highlight = predictionResponse.data.originalFeminineWords;
-//     //     const m_table = predictionResponse.data.masculineWords;
-//     //     const f_table = predictionResponse.data.feminineWords;
-//     //     // Send a response
-//     //     res.status(200).send({
-//     //       message: "Text submitted successfully",
-//     //       userInput,
-//     //       malePercentage: m_percent,
-//     //       femalePercentage: f_percent,
-//     //       maleHighlight: m_highlight,
-//     //       femaleHighlight: f_highlight,
-//     //       maleTable: m_table,
-//     //       femaleTable: f_table,
-//     //     });
-//     //   } else if (scrollBar === 1) {
-//     //     // scrollBar is at Political
-//     //     // Print liberal-conservative log messages
-
-//     //     // REPLACE BELOW
-//     //     console.log("Male Percentage:", predictionResponse.data.malePercentage);
-//     //     console.log("Masculine Words", predictionResponse.data.masculineWords);
-//     //     console.log(
-//     //       "Female Percentage:",
-//     //       predictionResponse.data.femalePercentage
-//     //     );
-//     //     console.log("Feminine Words", predictionResponse.data.feminineWords);
-//     //     console.log(
-//     //       "Original Masculine Words:",
-//     //       predictionResponse.data.originalMasculineWords
-//     //     );
-//     //     console.log(
-//     //       "Original Feminine Words:",
-//     //       predictionResponse.data.originalFeminineWords
-//     //     );
-
-//     //     const m_percent = predictionResponse.data.malePercentage * 100;
-//     //     const f_percent = predictionResponse.data.femalePercentage * 100;
-//     //     const m_highlight = predictionResponse.data.originalMasculineWords;
-//     //     const f_highlight = predictionResponse.data.originalFeminineWords;
-//     //     const m_table = predictionResponse.data.masculineWords;
-//     //     const f_table = predictionResponse.data.feminineWords;
-//     //     // Send a response
-//     //     res.status(200).send({
-//     //       message: "Text submitted successfully",
-//     //       userInput,
-//     //       malePercentage: m_percent,
-//     //       femalePercentage: f_percent,
-//     //       maleHighlight: m_highlight,
-//     //       femaleHighlight: f_highlight,
-//     //       maleTable: m_table,
-//     //       femaleTable: f_table,
-//     //     });
-
-//     //     // console.log("Liberal:", liberal);
-//     //     // console.log("Conservative:", conservative);
-//     //   } else {
-//     //     console.log(
-//     //       "Invalid scrollBar value. We should not see this message as scrollBar is always 0 or 1."
-//     //     );
-//     //   }
-//     // } else {
-//     //   console.log("scrollBar variable is not defined.");
-//     //   res.status(500).send({ message: "Error" });
-//     // }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ message: "Error" });
-//   }
-// });
+const functions = require("firebase-functions");
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const { readFile } = require("fs").promises;
+
+// firebase
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "unbiastext.firebaseapp.com",
+  projectId: "unbiastext",
+  storageBucket: "unbiastext.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 const app = express();
 
@@ -170,13 +27,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  res.send(await readFile("./home.html", "utf8"));
+  res.send(await readFile("./public/home.html", "utf8"));
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App is available on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App is available on http://localhost:${PORT}`);
+// });
 
 app.post("/", async (req, res) => {
   const userInput = req.body.text; // Access the text input from the request body
@@ -287,3 +144,5 @@ app.post("/", async (req, res) => {
     }
   }
 });
+
+exports.app = functions.https.onRequest(firebaseApp);
